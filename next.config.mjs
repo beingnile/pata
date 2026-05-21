baafa23a4eefa085d45b29f853abe087a1bfe476
beingnile <okomonile3@gmail.com>
2026-05-21 12:38:59 +0300

    Update mdx

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,12 @@ const nextConfig = {
   /* config options here */
 };
 
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+});
+
 export default nextConfig;
